0bc8346490b1a8081479644b943fb6371d236188
FrozenIce <icecastle221@gmail.com>
2017-04-27 17:09:08 -0400

    Add type to name.

--- Engine/DirectX11_Starter/ClassTypeId.h
+++ Engine/DirectX11_Starter/ClassTypeId.h
@@ -25,6 +25,8 @@
 
 #pragma once
 #include <cstddef>
+#include <map>
+#include <iostream>
 
 typedef std::size_t TypeId;
 
@@ -36,14 +38,22 @@ public:
 	template <typename T>
 	static TypeId GetTypeId()
 	{
-		static const TypeId id = m_nextTypeId++;
+		static const TypeId id = (m_namesMap[m_nextTypeId] = const_cast<char*>(typeid(T).name()), m_nextTypeId++);
 		return id;
 	}
 
-private:
+	static char* GetTypeName(TypeId id)
+	{
+		return m_namesMap[id];
+	}
 
+private:
 	static TypeId m_nextTypeId;
+	static std::map<TypeId, char*> m_namesMap;
 };
 
 template <typename TBase>
 TypeId ClassTypeId<TBase>::m_nextTypeId = 0;
+
+template <typename TBase>
+std::map<TypeId, char*> ClassTypeId<TBase>::m_namesMap;
