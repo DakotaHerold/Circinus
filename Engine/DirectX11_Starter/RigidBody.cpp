0c1819b0367edf7991b42d20be20021f53eb3170
Rahul Madhyan (RIT Student) <rm7755@ad.rit.edu>
2017-05-11 15:51:57 -0400

    RigidBody Class

--- Engine/DirectX11_Starter/RigidBody.cpp
+++ Engine/DirectX11_Starter/RigidBody.cpp
@@ -7,8 +7,6 @@ RigidBody::RigidBody(Transform* t, const DirectX::BoundingBox* boxCollider)
 	trans = t; 
 	obb = new BoundingOrientedBox(); 
 	DirectX::BoundingOrientedBox::CreateFromBoundingBox(*obb, *boxCollider);
-
-	XMFLOAT3* velocity = new XMFLOAT3(0, 0, 0);
 }
 
 
@@ -22,27 +20,27 @@ void RigidBody::SetEntity(int id)
 	this->Component::SetEntity(id);
 	// TODO: Update with transform position
 }
-//
-//bool RigidBody::SphereCollisionCheck(RigidBody *otherRbody)
-//{
-//	const BoundingBox &box1 = *(obb);
-//	BoundingSphere sphere1;
-//	BoundingSphere::CreateFromBoundingBox(sphere1, box1);
-//	XMFLOAT3 rBody1Position = *(trans->GetLocalPosition());
-//	sphere1.Center.x += rBody1Position.x;
-//	sphere1.Center.y += rBody1Position.y;
-//	sphere1.Center.z += rBody1Position.z;
-//
-//	const BoundingBox &box2 = *(otherRbody->obb);
-//	BoundingSphere sphere2;
-//	BoundingSphere::CreateFromBoundingBox(sphere2, box2);
-//	XMFLOAT3 rBody2Position = *(otherRbody->GetTransform()->GetLocalPosition());
-//	sphere2.Center.x += rBody2Position.x;
-//	sphere2.Center.y += rBody2Position.y;
-//	sphere2.Center.z += rBody2Position.z;
-//
-//	return sphere1.Intersects(sphere2);
-//}
+
+bool RigidBody::SphereCollisionCheck(RigidBody *otherRbody)
+{
+	const BoundingOrientedBox &box1 = *(obb);
+	BoundingSphere sphere1;
+	BoundingSphere::CreateFromBoundingBox(sphere1, box1);
+	XMFLOAT3 rBody1Position = *(trans->GetLocalPosition());
+	sphere1.Center.x += rBody1Position.x;
+	sphere1.Center.y += rBody1Position.y;
+	sphere1.Center.z += rBody1Position.z;
+
+	const BoundingOrientedBox &box2 = *(otherRbody->obb);
+	BoundingSphere sphere2;
+	BoundingSphere::CreateFromBoundingBox(sphere2, box2);
+	XMFLOAT3 rBody2Position = *(otherRbody->GetTransform()->GetLocalPosition());
+	sphere2.Center.x += rBody2Position.x;
+	sphere2.Center.y += rBody2Position.y;
+	sphere2.Center.z += rBody2Position.z;
+
+	return sphere1.Intersects(sphere2);
+}
 
 bool RigidBody::BoxCollisionCheck(RigidBody * otherRbody)
 {
@@ -56,21 +54,5 @@ bool RigidBody::BoxCollisionCheck(RigidBody * otherRbody)
 	otherRbody->obb->Center.y = pos2->y;
 	otherRbody->obb->Center.z = pos2->z;
 
-
 	return obb->Intersects(*(otherRbody->obb));
-}
-
-void RigidBody::Orbiting(RigidBody * orbitee)
-{
-	XMFLOAT3* orbiterPos = trans->GetLocalPosition();
-	obb->Center.x = orbiterPos->x;
-	obb->Center.x = orbiterPos->y;
-	obb->Center.x = orbiterPos->z;
-
-	XMFLOAT3* orbiteePos = orbitee->trans->GetLocalPosition();
-	orbitee->obb->Center.x = orbiteePos->x;
-	orbitee->obb->Center.y = orbiteePos->y;
-	orbitee->obb->Center.z = orbiteePos->z;
-
-
 }
