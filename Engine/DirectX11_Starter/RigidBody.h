bc5d692077ded437a12239efd6e259ca08d93ff1
Rahul Madhyan <rahulsmadhy@gmail.com>
2017-05-15 11:46:18 -0400

    RigidBody changed localposition to world position for collision checks

--- Engine/DirectX11_Starter/RigidBody.h
+++ Engine/DirectX11_Starter/RigidBody.h
@@ -29,6 +29,9 @@ public:
 	DirectX::XMFLOAT3 RotateToNewEuler(DirectX::XMFLOAT3X3* m);
 	DirectX::XMFLOAT3 velocity; // This velocity will be a vector3 relate to world space instead of local space.
 
+	void Orbit(RigidBody *otherBody, float radius, float speed, float deltaTime);
+	void Seek(RigidBody *otherBody, float speed);
+
 	void	Serialize(rapidjson::PrettyWriter<rapidjson::StringBuffer>& writer) {
 		writer.StartObject();
 		writer.String("name");
